d2308e10bd22c68ae67f1294b9bc55b33d5d77b8
Huangmj <3220837922@qq.com>
2025-06-10 19:38:14 +0800

    optimize argument

--- run.sh
+++ run.sh
@@ -25,7 +25,8 @@ jmeter -n -t "${TEST_FILE}" \
        -l "${RESULT_FILE}" \
        -e -o "${REPORT_DIR}" \
        -Jthreads=${THREADS} \
-       -Jrampup=${RAMPUP}  
+       -Jrampup=${RAMPUP}  \
+       -Xms4g -Xmx8g
 
 
 # 记录结束时间
